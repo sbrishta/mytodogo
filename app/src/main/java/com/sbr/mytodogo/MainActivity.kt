8d1f2f440972175b4b7665e210dd2dca7045d80c
sbrishta <sbrishta.13@gmail.com>
2022-01-12 01:13:22 +0800

    Implemented Bottom navigation view with Navigation graph. - added bottom navigation view - added related menu items - added navigation graph todo_nav.xml  - added to do list and notes fragment - in main activity integrated nav controller with bottom navigation view - also added action bar configuration to show changes on title bar according to navigation item

--- app/src/main/java/com/sbr/mytodogo/MainActivity.kt
+++ app/src/main/java/com/sbr/mytodogo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.sbr.mytodogo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val todoFragmentList = TodoListFragment()
+        val notesListFragment = NotesListFragment()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfig = AppBarConfiguration(setOf(R.id.todoListFragment,R.id.notesListFragment))
+        setupActionBarWithNavController(navController,appBarConfig)
     }
+
 }
